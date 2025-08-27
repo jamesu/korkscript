b141b3ed67b5a998601cd20d872d6c6d24d82f40
James Urquhart <jamesu@gmail.com>
2025-08-27 18:57:36 +0100

    Fix typo

--- test/astPrint.cpp
+++ test/astPrint.cpp
@@ -33,15 +33,15 @@ static void printNode(const StmtNode* n, int pad = 0);
 
 template <class T>
 static void printChild(const char* key, const T* child, int pad) {
-   indent(pad); rintf("%s = ", key);
+   indent(pad); printf("%s = ", key);
    if (!child) { uts("null"); return; }
    printNode(child, pad);
 }
 
 template <class T>
 static void printList(const char* key, const T* head, int pad) {
-   if (!head) { indent(pad); rintf("%s = null\n", key); return; }
-   indent(pad); rintf("%s = {\n", key);
+   if (!head) { indent(pad); printf("%s = null\n", key); return; }
+   indent(pad); printf("%s = {\n", key);
    const StmtNode* it = head;
    while (it) {
       printNode(it, pad + 2);
@@ -63,7 +63,7 @@ static const char* typeReqName(TypeReq t) {
 }
 
 static void open(const char* klass, int pad) {
-   indent(pad); rintf("%s {\n", klass);
+   indent(pad); printf("%s {\n", klass);
 }
 
 static void close(const StmtNode* n, int pad) {
@@ -97,8 +97,8 @@ static void printNode(const StmtNode* n, int pad) {
       printChild("testExpr", x->testExpr, pad + 2);
       printChild("ifBlock",  x->ifBlock,  pad + 2);
       printChild("elseBlock",x->elseBlock,pad + 2);
-      indent(pad + 2); rintf("integer = %s\n",   yesno(x->integer));
-      indent(pad + 2); rintf("propagate = %s\n", yesno(x->propagate));
+      indent(pad + 2); printf("integer = %s\n",   yesno(x->integer));
+      indent(pad + 2); printf("propagate = %s\n", yesno(x->propagate));
       close(n, pad);
       return;
    }
@@ -108,23 +108,23 @@ static void printNode(const StmtNode* n, int pad) {
       printChild("initExpr",   x->initExpr,   pad + 2);
       printChild("endLoopExpr",x->endLoopExpr,pad + 2);
       printChild("loopBlock",  x->loopBlock,  pad + 2);
-      indent(pad + 2); rintf("isDoLoop = %s\n", yesno(x->isDoLoop));
-      indent(pad + 2); rintf("integer = %s\n",  yesno(x->integer));
+      indent(pad + 2); printf("isDoLoop = %s\n", yesno(x->isDoLoop));
+      indent(pad + 2); printf("integer = %s\n",  yesno(x->integer));
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const IterStmtNode*>(n)) {
       open("IterStmtNode", pad);
-      indent(pad + 2); rintf("varName = \"%s\"\n", show(x->varName));
+      indent(pad + 2); printf("varName = \"%s\"\n", show(x->varName));
       printChild("containerExpr", x->containerExpr, pad + 2);
       printChild("body",          x->body,          pad + 2);
-      indent(pad + 2); rintf("isStringIter = %s\n", yesno(x->isStringIter));
+      indent(pad + 2); printf("isStringIter = %s\n", yesno(x->isStringIter));
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const TTagSetStmtNode*>(n)) {
       open("TTagSetStmtNode", pad);
-      indent(pad + 2); rintf("tag = \"%s\"\n", show(x->tag));
+      indent(pad + 2); printf("tag = \"%s\"\n", show(x->tag));
       printChild("valueExpr",  x->valueExpr,  pad + 2);
       printChild("stringExpr", x->stringExpr, pad + 2);
       close(n, pad);
@@ -132,10 +132,10 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const FunctionDeclStmtNode*>(n)) {
       open("FunctionDeclStmtNode", pad);
-      indent(pad + 2); rintf("fnName = \"%s\"\n",    show(x->fnName));
-      indent(pad + 2); rintf("nameSpace = \"%s\"\n", show(x->nameSpace));
-      indent(pad + 2); rintf("package = \"%s\"\n",   show(x->package));
-      indent(pad + 2); rintf("argc = %i\n", x->argc);
+      indent(pad + 2); printf("fnName = \"%s\"\n",    show(x->fnName));
+      indent(pad + 2); printf("nameSpace = \"%s\"\n", show(x->nameSpace));
+      indent(pad + 2); printf("package = \"%s\"\n",   show(x->package));
+      indent(pad + 2); printf("argc = %i\n", x->argc);
       printList("args",  x->args,  pad + 2);
       printList("stmts", x->stmts, pad + 2);
       close(n, pad);
@@ -148,13 +148,13 @@ static void printNode(const StmtNode* n, int pad) {
       printChild("testExpr",  x->testExpr,  pad + 2);
       printChild("trueExpr",  x->trueExpr,  pad + 2);
       printChild("falseExpr", x->falseExpr, pad + 2);
-      indent(pad + 2); rintf("integer = %s\n", yesno(x->integer));
+      indent(pad + 2); printf("integer = %s\n", yesno(x->integer));
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const FloatBinaryExprNode*>(n)) {
       open("FloatBinaryExprNode", pad);
-      indent(pad + 2); rintf("op = %d\n", x->op);
+      indent(pad + 2); printf("op = %d\n", x->op);
       printChild("left",  x->left,  pad + 2);
       printChild("right", x->right, pad + 2);
       close(n, pad);
@@ -162,9 +162,9 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const IntBinaryExprNode*>(n)) {
       open("IntBinaryExprNode", pad);
-      indent(pad + 2); rintf("op = %d\n", x->op);
-      indent(pad + 2); rintf("subType = %s\n", typeReqName(x->subType));
-      indent(pad + 2); rintf("operand = %u\n", x->operand);
+      indent(pad + 2); printf("op = %d\n", x->op);
+      indent(pad + 2); printf("subType = %s\n", typeReqName(x->subType));
+      indent(pad + 2); printf("operand = %u\n", x->operand);
       printChild("left",  x->left,  pad + 2);
       printChild("right", x->right, pad + 2);
       close(n, pad);
@@ -172,7 +172,7 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const StreqExprNode*>(n)) {
       open("StreqExprNode", pad);
-      indent(pad + 2); rintf("eq = %s\n", yesno(x->eq));
+      indent(pad + 2); printf("eq = %s\n", yesno(x->eq));
       printChild("left",  x->left,  pad + 2);
       printChild("right", x->right, pad + 2);
       close(n, pad);
@@ -180,7 +180,7 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const StrcatExprNode*>(n)) {
       open("StrcatExprNode", pad);
-      indent(pad + 2); rintf("appendChar = %d\n", x->appendChar);
+      indent(pad + 2); printf("appendChar = %d\n", x->appendChar);
       printChild("left",  x->left,  pad + 2);
       printChild("right", x->right, pad + 2);
       close(n, pad);
@@ -195,56 +195,56 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const IntUnaryExprNode*>(n)) {
       open("IntUnaryExprNode", pad);
-      indent(pad + 2); rintf("op = %d\n", x->op);
-      indent(pad + 2); rintf("integer = %s\n", yesno(x->integer));
+      indent(pad + 2); printf("op = %d\n", x->op);
+      indent(pad + 2); printf("integer = %s\n", yesno(x->integer));
       printChild("expr", x->expr, pad + 2);
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const FloatUnaryExprNode*>(n)) {
       open("FloatUnaryExprNode", pad);
-      indent(pad + 2); rintf("op = %d\n", x->op);
+      indent(pad + 2); printf("op = %d\n", x->op);
       printChild("expr", x->expr, pad + 2);
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const VarNode*>(n)) {
       open("VarNode", pad);
-      indent(pad + 2); rintf("varName = \"%s\"\n", show(x->varName));
+      indent(pad + 2); printf("varName = \"%s\"\n", show(x->varName));
       printChild("arrayIndex", x->arrayIndex, pad + 2);
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const IntNode*>(n)) {
       open("IntNode", pad);
-      indent(pad + 2); rintf("value = %d\n", x->value);
+      indent(pad + 2); printf("value = %d\n", x->value);
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const FloatNode*>(n)) {
       open("FloatNode", pad);
-      indent(pad + 2); rintf("value = %g\n", x->value);
+      indent(pad + 2); printf("value = %g\n", x->value);
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const StrConstNode*>(n)) {
       open("StrConstNode", pad);
-      indent(pad + 2); rintf("str = \"%s\"\n", show(x->str));
-      indent(pad + 2); rintf("tag = %s\n", yesno(x->tag));
-      indent(pad + 2); rintf("doc = %s\n", yesno(x->doc));
+      indent(pad + 2); printf("str = \"%s\"\n", show(x->str));
+      indent(pad + 2); printf("tag = %s\n", yesno(x->tag));
+      indent(pad + 2); printf("doc = %s\n", yesno(x->doc));
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const ConstantNode*>(n)) {
       open("ConstantNode", pad);
-      indent(pad + 2); rintf("value = \"%s\"\n", show(x->value));
+      indent(pad + 2); printf("value = \"%s\"\n", show(x->value));
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const AssignExprNode*>(n)) {
       open("AssignExprNode", pad);
-      indent(pad + 2); rintf("varName = \"%s\"\n", show(x->varName));
-      indent(pad + 2); rintf("subType = %s\n",    typeReqName(x->subType));
+      indent(pad + 2); printf("varName = \"%s\"\n", show(x->varName));
+      indent(pad + 2); printf("subType = %s\n",    typeReqName(x->subType));
       printChild("arrayIndex", x->arrayIndex, pad + 2);
       printChild("expr",       x->expr,       pad + 2);
       close(n, pad);
@@ -252,10 +252,10 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const AssignOpExprNode*>(n)) {
       open("AssignOpExprNode", pad);
-      indent(pad + 2); rintf("varName = \"%s\"\n", show(x->varName));
-      indent(pad + 2); rintf("op = %d\n",          x->op);
-      indent(pad + 2); rintf("operand = %i\n", x->operand);
-      indent(pad + 2); rintf("subType = %s\n",     typeReqName(x->subType));
+      indent(pad + 2); printf("varName = \"%s\"\n", show(x->varName));
+      indent(pad + 2); printf("op = %d\n",          x->op);
+      indent(pad + 2); printf("operand = %i\n", x->operand);
+      indent(pad + 2); printf("subType = %s\n",     typeReqName(x->subType));
       printChild("arrayIndex", x->arrayIndex, pad + 2);
       printChild("expr",       x->expr,       pad + 2);
       close(n, pad);
@@ -269,22 +269,22 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const TTagExprNode*>(n)) {
       open("TTagExprNode", pad);
-      indent(pad + 2); rintf("tag = \"%s\"\n", show(x->tag));
+      indent(pad + 2); printf("tag = \"%s\"\n", show(x->tag));
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const FuncCallExprNode*>(n)) {
       open("FuncCallExprNode", pad);
-      indent(pad + 2); rintf("funcName = \"%s\"\n", show(x->funcName));
-      indent(pad + 2); rintf("nameSpace = \"%s\"\n", show(x->nameSpace));
-      indent(pad + 2); rintf("callType = %i\n", x->callType);
+      indent(pad + 2); printf("funcName = \"%s\"\n", show(x->funcName));
+      indent(pad + 2); printf("nameSpace = \"%s\"\n", show(x->nameSpace));
+      indent(pad + 2); printf("callType = %i\n", x->callType);
       printList("args", x->args, pad + 2);
       close(n, pad);
       return;
    }
    if (auto x = dynamic_cast<const SlotAccessNode*>(n)) {
       open("SlotAccessNode", pad);
-      indent(pad + 2); rintf("slotName = \"%s\"\n", show(x->slotName));
+      indent(pad + 2); printf("slotName = \"%s\"\n", show(x->slotName));
       printChild("objectExpr", x->objectExpr, pad + 2);
       printChild("arrayExpr",  x->arrayExpr,  pad + 2);
       close(n, pad);
@@ -292,7 +292,7 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const InternalSlotAccessNode*>(n)) {
       open("InternalSlotAccessNode", pad);
-      indent(pad + 2); rintf("recurse = %s\n", yesno(x->recurse));
+      indent(pad + 2); printf("recurse = %s\n", yesno(x->recurse));
       printChild("objectExpr", x->objectExpr, pad + 2);
       printChild("slotExpr",   x->slotExpr,   pad + 2);
       close(n, pad);
@@ -300,8 +300,8 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const SlotAssignNode*>(n)) {
       open("SlotAssignNode", pad);
-      indent(pad + 2); rintf("slotName = \"%s\"\n", show(x->slotName));
-      indent(pad + 2); rintf("typeID = %i\n", x->typeID);
+      indent(pad + 2); printf("slotName = \"%s\"\n", show(x->slotName));
+      indent(pad + 2); printf("typeID = %i\n", x->typeID);
       printChild("objectExpr", x->objectExpr, pad + 2);
       printChild("arrayExpr",  x->arrayExpr,  pad + 2);
       printChild("valueExpr",  x->valueExpr,  pad + 2);
@@ -310,10 +310,10 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const SlotAssignOpNode*>(n)) {
       open("SlotAssignOpNode", pad);
-      indent(pad + 2); rintf("slotName = \"%s\"\n", show(x->slotName));
-      indent(pad + 2); rintf("op = %d\n", x->op);
-      indent(pad + 2); rintf("operand = %i\n", x->operand);
-      indent(pad + 2); rintf("subType = %s\n", typeReqName(x->subType));
+      indent(pad + 2); printf("slotName = \"%s\"\n", show(x->slotName));
+      indent(pad + 2); printf("op = %d\n", x->op);
+      indent(pad + 2); printf("operand = %i\n", x->operand);
+      indent(pad + 2); printf("subType = %s\n", typeReqName(x->subType));
       printChild("objectExpr", x->objectExpr, pad + 2);
       printChild("arrayExpr",  x->arrayExpr,  pad + 2);
       printChild("valueExpr",  x->valueExpr,  pad + 2);
@@ -322,10 +322,10 @@ static void printNode(const StmtNode* n, int pad) {
    }
    if (auto x = dynamic_cast<const ObjectDeclNode*>(n)) {
       open("ObjectDeclNode", pad);
-      indent(pad + 2); rintf("parentObject = \"%s\"\n", show(x->parentObject));
-      indent(pad + 2); rintf("isDatablock = %s\n", yesno(x->isDatablock));
-      indent(pad + 2); rintf("isClassNameInternal = %s\n", yesno(x->isClassNameInternal));
-      indent(pad + 2); rintf("isSingleton = %s\n", yesno(x->isSingleton));
+      indent(pad + 2); printf("parentObject = \"%s\"\n", show(x->parentObject));
+      indent(pad + 2); printf("isDatablock = %s\n", yesno(x->isDatablock));
+      indent(pad + 2); printf("isClassNameInternal = %s\n", yesno(x->isClassNameInternal));
+      indent(pad + 2); printf("isSingleton = %s\n", yesno(x->isSingleton));
       printChild("classNameExpr", x->classNameExpr, pad + 2);
       printChild("objectNameExpr", x->objectNameExpr, pad + 2);
       printList ("argList",       x->argList,       pad + 2);
