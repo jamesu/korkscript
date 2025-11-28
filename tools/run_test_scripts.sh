0cd266d7a77f9b1403cc56f27dd34d41f2c12bca
James Urquhart <jamesu@gmail.com>
2025-11-28 22:49:11 +0000

    Five million attempts later

--- tools/run_test_scripts.sh
+++ tools/run_test_scripts.sh
@@ -5,14 +5,14 @@ ROOT_DIR="$(git rev-parse --show-toplevel)"
 cd "$ROOT_DIR"
 
 CURRENT_BUILD_DIR="${CURRENT_BUILD_DIR:-test_build}"
-TEST_SCRIPTS_DIR="${TEST_SCRIPTS_DIR:-test/script_tests}"
+UNIT_TEST_SCRIPTS_DIR="${UNIT_TEST_SCRIPTS_DIR:-test/script_tests}"
 TEST_RUNNER_BIN_NAME="${TEST_RUNNER_BIN_NAME:-testrunner}"
 CURRENT_BUILD_DIR="${CURRENT_BUILD_DIR:-test_build}"
 TEST_RUNNER_BIN="$ROOT_DIR/$CURRENT_BUILD_DIR/$TEST_RUNNER_BIN_NAME"
 
 while IFS= read -r -d '' script; do
-  rel="${script#$TEST_SCRIPTS_DIR/}"
+  rel="${script#$UNIT_TEST_SCRIPTS_DIR/}"
   echo "== Testing: $rel =="
 
   "$TEST_RUNNER_BIN" "$script"
-done < <(find "$TEST_SCRIPTS_DIR" -type f \( -name '*.cs' -o -name '*.tscript' \) -print0)
+done < <(find "$UNIT_TEST_SCRIPTS_DIR" -type f \( -name '*.cs' -o -name '*.tscript' \) -print0)
